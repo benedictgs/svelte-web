578f7f46c0d8207705a5179c20081925fe9a76d7
Benedict <benedict.gs@gmail.com>
2023-04-08 19:59:02 +0700

    clean unused code

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -48,24 +48,3 @@
         </ul>
     </nav>
 </header>
-
-
-<style>
-/*     
-    ul {
-        padding: 0;
-        margin: 0;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        list-style: none;
-    }
-
-    li {
-        position: relative;
-    }
-
-    nav a {
-        padding: 0 0.5em;
-    } */
-</style>
