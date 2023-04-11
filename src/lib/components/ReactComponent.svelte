55c7eacd52ca5dad19534b8a774e4d6c1d0067a1
Benedict <benedict.gs@gmail.com>
2023-04-11 18:32:36 +0700

    React Component in Svelte

---
+++ src/lib/components/ReactComponent.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import React from "react";
+    import ReactDOM from "react-dom";
+    import { onDestroy, afterUpdate } from "svelte";
+    const e = React.createElement;
+    let container: HTMLDivElement;
+    /**
+     * Svelte compiles on the server/statically, React renders on the client.
+     * Therefore, React stuff need to happen within Svelte (window/dom) zones.
+     */
+    afterUpdate(() => {
+      const { el, children, class: _, ...props } = $$props;
+      try {
+        ReactDOM.render(e(el, props, children), container);
+      } catch (err) {
+        console.warn(`react-adapter failed to mount.`, { err });
+      }
+    });
+    onDestroy(() => {
+      try {
+        container && ReactDOM.unmountComponentAtNode(container);
+      } catch (err) {
+        console.warn(`react-adapter failed to unmount.`, { err });
+      }
+    });
+  </script>
+  
+  <div bind:this={container} class={$$props.class} />
