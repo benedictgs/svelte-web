d02f4971e3e8e2e5b277361724b23f10746fa062
Benedict <benedict.gs@gmail.com>
2023-04-17 17:28:14 +0700

    Add logo on about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
     import type { PageData } from './$types';
+
+    import SvelteLogo from '$lib/media/svelte-logo.svg';
+    import TypescriptLogo from '$lib/media/typescript-logo.svg';
+    import TailwindLogo from '$lib/media/tailwind-logo.svg';
+    import GithubLogo from '$lib/media/github-logo.svg';
     
     export let data: PageData;
+
+    let Logos = [
+        {src:SvelteLogo, alt:"svelte logo"},
+        {src:TypescriptLogo, alt:"Typescript logo"},
+        {src:TailwindLogo, alt:"Tailwind logo"},
+        {src:GithubLogo, alt:"Github logo"},
+    ]
+
 </script>
 
 <div class="absolute inset-0 z-0 bg-cover bg-center bg-no-repeat bg-[#0B1120] bg-[url('$lib/media/hero-dark.jpg')] blur-3xl"></div>
@@ -10,6 +23,12 @@
     <div class="z-10 flex flex-col items-center text-center lg:mt-20">
         <h1 class="text-7xl mx-10 mt-20 sm:text-9xl  font-lexend drop-shadow-[2px_10px_5px_hsla(200,100%,0%,50%)]">About</h1>
         <h2>This Website Tech Stack</h2>
-        <p class="mx-10 sm:mx-20 my-5 max-w-[75ch] font-light text-stone-200 md:text-xl">Svelte Typescript Tailwind Cloudflare Github </p>
+        <p class="mx-10 sm:mx-20 my-5 max-w-[75ch] font-light text-stone-200 md:text-xl">
+            Svelte Typescript Tailwind Cloudflare Github </p>
+        <div class="grid grid-flow-col gap-3 justify-center items-center">
+            {#each Logos as logo}
+                <img src={logo.src} alt={logo.alt} width="30" height="30">
+            {/each}
+        </div>
     </div>
 </div>
