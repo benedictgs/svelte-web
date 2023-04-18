6db21b0f3959a0e27d65ddea6790e719cffc6121
Benedict <benedict.gs@gmail.com>
2023-04-18 15:59:30 +0700

    transition tweak home section

--- src/lib/components/page/BragSection.svelte
+++ src/lib/components/page/BragSection.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { fade } from 'svelte/transition'
+    import { fade, fly } from 'svelte/transition'
 
     import logoAkltg from '$lib/media/logo-akltg-w.png';
     import logoItb from '$lib/media/logo-itb-w-s-p.png';
@@ -7,6 +7,7 @@
     import logoOpensap from '$lib/media/logo-opensap-w.png';
     import logoSuj from '$lib/media/logo-suj-w-s.png';
     import logoUnilever from '$lib/media/logo-unilever.png';
+	import IntersectionObserver from '../IntersectionObserver.svelte';
 
     let Logos = [
         {src: logoItb, alt: "logo itb" },
@@ -21,16 +22,24 @@
     
 </script>
 
-<section id={id} class="flex justify-center relative">
-    <div class="absolute top-0 inset-x-0 h-[20rem] [mask-image:linear-gradient(0deg,transparent,black)] bg-[url('$lib/media/one-small-grid.svg')] bg-[center_top_-1px] border-t border-slate-100/5 xl:top-8"></div>
-    <div class="w-full lg:max-w-screen-lg  flex flex-col bg-black/30 my-10 mx-3 p-6 md:px-6 rounded-3xl">
-        <h1 in:fade="{{delay:100,duration:1000}}" class="text-xl font-semibold">People call this Brag Section</h1>
-        <p in:fade="{{delay:600,duration:1000}}" class="text-xs text-slate-400 font-lexend"> i feel like i can't brag about this, although i did it anyway ¯\_(ツ)_/¯ </p>
-        
-        <div class="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-6 gap-5 items-center ">
-            {#each Logos as logo}
-                <img src="{logo.src}" alt="{logo.alt}">
-            {/each}
-        </div>
-    </div>
-</section>
+
+<IntersectionObserver let:intersecting>
+    {#if intersecting}
+        <section id={id} class="flex justify-center relative">
+            <div class="absolute top-0 inset-x-0 h-[20rem] [mask-image:linear-gradient(0deg,transparent,black)] bg-[url('$lib/media/one-small-grid.svg')] bg-[center_top_-1px] border-t border-slate-100/5 xl:top-8"></div>
+            <div class="w-full lg:max-w-screen-lg  flex flex-col bg-black/30 my-10 mx-3 p-6 md:px-6 rounded-3xl">
+                <h1 in:fade="{{delay:100,duration:1000}}" class="text-xl font-semibold">People call this Brag Section</h1>
+                <p in:fade="{{delay:600,duration:1000}}" class="text-xs text-slate-400 font-lexend"> i feel like i can't brag about this, although i did it anyway ¯\_(ツ)_/¯ </p>
+                
+                <div class="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-6 gap-5 items-center ">
+                    {#each Logos as logo, i}
+                    <img in:fly="{{delay: 700+(i*200), duration:1000, y:50}}" src="{logo.src}" alt="{logo.alt}">
+                    {/each}
+                </div>
+            </div>
+        </section>
+    {:else}
+        <section id={id} class="flex justify-center relative"></section>
+    {/if}
+
+</IntersectionObserver>
--- src/lib/components/page/HeroSection.svelte
+++ src/lib/components/page/HeroSection.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { fade,fly } from "svelte/transition";
+    import { fade, fly } from "svelte/transition";
     import CountryFlag from '$lib/media/indonesia-flag.png';
 	import RadarContainer from "../chart/RadarContainer.svelte";
 	import LineAreaContainer from "../chart/LineAreaContainer.svelte";
@@ -54,10 +54,10 @@
 
 <IntersectionObserver let:intersecting>
     {#if intersecting}
-<section id={id} class="flex justify-center bg-cover bg-top bg-no-repeat bg-fixed bg-[url('$lib/media/background-hero-blur.webp')] pt-4 ">
-    <div in:fly="{{y:200, duration:500}}" class="w-full lg:max-w-screen-lg mb-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 lg:gap-10 bg-black/50  mx-3 p-6 md:px-6 rounded-xl">
-                <div in:fade={{delay:600,duration:1000}} class="flex flex-col justify-between sm:max-w-sm row-span-2 lg:col-span-1">
-                    <div class="flex flex-row hover:animate-pulse">
+        <section id={id} class="flex justify-center bg-cover bg-top bg-no-repeat bg-fixed bg-[url('$lib/media/background-hero-blur.webp')] pt-4 ">
+            <div class="w-full lg:max-w-screen-lg mb-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 lg:gap-10 bg-black/50  mx-3 p-6 md:px-6 rounded-xl">
+                <div class="flex flex-col justify-between sm:max-w-sm row-span-2 lg:col-span-1">
+                    <div in:fade={{delay:600,duration:1000}} class="flex flex-row hover:animate-pulse">
                         <div class="sm:hidden grow-0 max-w-[10rem]">
                             <img loading="lazy" src={PotraitProfileMini} alt="Profile" class="  ">
                         </div>
@@ -69,10 +69,10 @@
                             <h2>Creative Tech Enthusiast</h2>
                         </div>
                     </div>
-                    <picture class=" hidden sm:grid justify-center hover:animate-pulse">
+                    <picture in:fly="{{delay:1000,duration:2000, y:50}}" class=" hidden sm:grid justify-center hover:animate-pulse">
                         <img loading="lazy" src={PotraitProfile} alt="Profile" class="max-h-[40rem] ">
                     </picture>
-                    <div id="stats" class="grid grid-cols-3 place-items-center p-3">
+                    <div in:fly="{{delay:1200,duration:2000, y:50}}" id="stats" class="grid grid-cols-3 place-items-center p-3">
                         <div class="flex flex-col text-center hover:animate-pulse"> <p class="font-bebas text-4xl">{age}</p> <p class="uppercase">yr</p></div>
                         <div class="flex flex-col text-center hover:animate-pulse"> <p class="font-bebas text-4xl">77</p> <p class="uppercase">kg</p></div>
                         <div class="flex flex-col text-center hover:animate-pulse"> <p class="font-bebas text-4xl">177</p> <p class="uppercase">cm</p></div>
@@ -81,7 +81,7 @@
                 <div>
                     <hr class=" my-3  border-gray-600">
                     <h3 in:fade="{{delay:600, duration:1000}}" class="font-bebas text-xl hover:animate-pulse">Personal Details</h3>
-                    <ul in:fade="{{delay:1000,duration:1000}}" class="">
+                    <ul in:fly="{{delay:1000,duration:2000, y:50}}" class="">
                         {#each stats as stat}
                         <li class="hover:animate-pulse"><span class="font-light">{stat.key} : </span><span class="font-semibold">{stat.value}</span></li>
                         {/each}
@@ -90,12 +90,16 @@
                 <div class="flex flex-col">
                     <hr class=" my-3  border-gray-600">
                     <h3 in:fade="{{delay:600,duration:1000}}" class="font-bebas text-xl hover:animate-pulse">Chart</h3>
-                    <div in:fade="{{delay:1000,duration:1000}}" class=" min-h-[10rem] hover:animate-pulse"><RadarContainer/></div>
+                    <div in:fly="{{delay:1000,duration:2000, y:50}}" class=" min-h-[10rem] hover:animate-pulse">
+                        <RadarContainer/>
+                    </div>
                 </div>
                 <div class="sm:col-span-2 flex flex-col">
                     <hr class=" my-3  border-gray-600">
                     <h3 in:fade="{{delay:600,duration:1000}}" class="font-bebas text-xl mb-3 hover:animate-pulse">Timeline of Performance</h3>
-                    <div in:fade="{{delay:1000,duration:1000}}" class="min-h-[10rem] hover:animate-pulse"><LineAreaContainer/></div>
+                    <div in:fly="{{delay:1200,duration:2000, y:50}}" class="min-h-[10rem] hover:animate-pulse">
+                        <LineAreaContainer/>
+                    </div>
                 </div>
 
             </div>
