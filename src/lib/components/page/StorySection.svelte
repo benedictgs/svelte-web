1a5622e9e7c9d99b4f044f3aac0f45e0d663aeaf
Benedict <benedict.gs@gmail.com>
2023-04-21 19:26:23 +0700

    Story small change

--- src/lib/components/page/StorySection.svelte
+++ src/lib/components/page/StorySection.svelte
@@ -5,6 +5,7 @@
     import motoris from '$lib/media/motoris-product.jpg';
     import globalcoco from '$lib/media/coco-factory.webp';
     import refinedSugar from '$lib/media/process-intern.webp';
+    import studentSenate from '$lib/media/student-senate.webp';
 
     export let id:string = 'story';
     
@@ -25,7 +26,7 @@
         <div class="w-full col-span-3">
 
             <ScrollShow >
-                <StoryCard alternate={true} year="2022" journey="motoris" imgSrc={motoris} summary="Automotive accessories e-commerce merchant"></StoryCard>
+                <StoryCard alternate={true} year="2022" journey="motoris" imgSrc={motoris} summary="Automotive e-commerce merchant"></StoryCard>
             </ScrollShow>
         </div>
         
@@ -63,15 +64,37 @@
         <div class="w-full col-span-3">
             <ScrollShow fly="right">
                 <StoryCard alternate={true} year="2018" journey="refined sugar" imgSrc={refinedSugar} summary="Process Management Intern">
-
+                    
                 </StoryCard>
             </ScrollShow>
         </div>
         
         <div class="w-full h-full col-start-4 row-start-5">
             <ScrollShow fly="scaleY">
-                <div  class=" mx-[1.5rem] my-[1rem] h-[20rem] p-1 bg-purple-400  rounded-full "> </div>
+                <div  class=" mx-[1.5rem] my-[1rem] h-[20rem] p-1 bg-indigo-400  rounded-full "> </div>
             </ScrollShow>
+        </div>
+        <div class="row-start-6 col-span-4 w-full ">
+            <ScrollShow fly="scaleX">
+                <div  class="my-[1.5rem] mx-[3rem] p-1  rounded-full bg-fuchsia-400 "></div>
+            </ScrollShow>            
+        </div>
+        <div class="w-full h-full col-start-1 row-start-7">
+            <ScrollShow fly="scaleY">
+                <div  class="ml-[1rem] mr-[1.5rem] my-[1rem] h-[20rem] p-1 bg-rose-300  rounded-full "> </div>
+            </ScrollShow>
+        </div>
+        <div class="w-full col-span-3">
+            <ScrollShow fly="left">
+                <StoryCard  year="2018" journey="Student Senate" imgSrc={studentSenate} summary="Financial Commitee Chairman">
+                    
+                </StoryCard>
+            </ScrollShow>
+        </div>
+        <div class=" col-span-4 w-full ">
+            <ScrollShow fly="up">
+                <div  class="my-[1.5rem] mx-[3rem] p-1  rounded-full bg-gray-400 "></div>
+            </ScrollShow>            
         </div>
         
         <div class="w-full col-span-4 hidden">
